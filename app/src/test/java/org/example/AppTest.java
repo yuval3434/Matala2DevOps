cb9dc716c7c445652d7489ef38490c2333ea9eeb
yuval3434 <yuval3401@gmail.com>
2026-09-25 16:36:53 +0300

    Remove broken getGreeting test

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -7,11 +7,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
-
     @Test void testAdd() {
         assertEquals(5, App.add(2, 3));
         assertEquals(-1, App.add(-2, 1));
